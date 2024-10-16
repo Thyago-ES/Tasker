8af078e4f550c43e9287b54cbd30924fe2cbb230
Thyago-ES <thyagorafael57@gmail.com>
2024-10-15 21:52:30 -0300

    feat: add page structure and a example tasks array to iterate tasks

--- src/pages/Area/index.tsx
+++ src/pages/Area/index.tsx
@@ -1,3 +1,46 @@
+import { MdDelete } from "react-icons/md";
+import { useLocation } from "react-router-dom";
+
+const todoList = [
+	{
+		id: 1,
+		title: "Estudar React hooks",
+	},
+	{
+		id: 2,
+		title: "Estudar React router",
+	},
+	{
+		id: 3,
+		title: "Estudar React icons",
+	},
+];
+
 export function Area() {
-	return <div>Area</div>;
+	const location = useLocation();
+
+	const { title } = location.state;
+
+	return (
+		<main>
+			<div>
+				<header>
+					<h1>{title}</h1>
+				</header>
+				<section>
+					<input type="text" placeholder="Qual a próxima tarefa?" />
+					<ul>
+						{todoList.map((todo) => (
+							<li key={todo.id}>
+								<span>{todo.title}</span>
+								<button>
+									<MdDelete />
+								</button>
+							</li>
+						))}
+					</ul>
+				</section>
+			</div>
+		</main>
+	);
 }
