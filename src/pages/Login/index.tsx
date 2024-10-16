0156c1838c85a3fa9d4a6099d674588954554cea
Thyago-ES <thyagorafael57@gmail.com>
2024-10-15 21:55:43 -0300

    feat: add page structure

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,3 +1,41 @@
+import { FormButton } from "../../components/FormButton";
+import { Title } from "../../components/Title";
+
 export function Login() {
-	return <div>Login</div>;
+	return (
+		<main>
+			<div>
+				<header>
+					<Title text="Entre agora" />
+
+					<p>
+						Organize as tarefas de hoje, seja produtivo e conclua suas metas.
+					</p>
+					<p>
+						Um pouco de tudo é melhor que muito de nada. Vamos fazer as tarefas
+						de hoje.
+					</p>
+				</header>
+
+				<section>
+					<form>
+						<div>
+							<label htmlFor="email">Email:</label>
+							<input type="email" id="email" placeholder="Digite seu email" />
+						</div>
+						<div>
+							<label htmlFor="password">Senha:</label>
+							<input
+								type="password"
+								id="password"
+								placeholder="Digite sua senha"
+							/>
+						</div>
+
+						<FormButton>Entrar</FormButton>
+					</form>
+				</section>
+			</div>
+		</main>
+	);
 }
