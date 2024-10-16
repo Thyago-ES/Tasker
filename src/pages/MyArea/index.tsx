28fa5a2a592e5925b95665b2ab4f8b2a5876b1d8
Thyago-ES <thyagorafael57@gmail.com>
2024-10-15 21:56:00 -0300

    feat: add page structure

--- src/pages/MyArea/index.tsx
+++ src/pages/MyArea/index.tsx
@@ -1,3 +1,69 @@
+import { FaPlus, FaArrowRight } from "react-icons/fa";
+
+import { Title } from "../../components/Title";
+import { useNavigate } from "react-router-dom";
+
+const areaList = [
+	{
+		id: 1,
+		title: "Estudar React",
+	},
+	{
+		id: 2,
+		title: "Estudar Node",
+	},
+	{
+		id: 3,
+		title: "Estudar Docker",
+	},
+	{
+		id: 4,
+		title: "Estudar Java",
+	},
+];
+
 export function MyArea() {
-	return <div>MyArea</div>;
+	const navigate = useNavigate();
+
+	return (
+		<main>
+			<div>
+				<header>
+					<Title text="Seja bem-vindo, Thyago!" />
+				</header>
+
+				<section>
+					<div>
+						<h3>Suas áreas de trabalho</h3>
+
+						<button className="btn btn__small btn__secondary">
+							<FaPlus />
+						</button>
+					</div>
+					<div>
+						{areaList.length === 0 ? (
+							<p>Não há áreas. Crie uma nova!</p>
+						) : (
+							areaList.map((area) => (
+								<div key={area.id}>
+									<h4>{area.title}</h4>
+									<button
+										onClick={() =>
+											navigate(`/thyago-rafael/area/${area.id}`, {
+												state: {
+													title: area.title,
+												},
+											})
+										}
+									>
+										<FaArrowRight />
+									</button>
+								</div>
+							))
+						)}
+					</div>
+				</section>
+			</div>
+		</main>
+	);
 }
