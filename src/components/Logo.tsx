c232feaf25c9e952b591c531d620c27cddd9304e
Thyago-ES <thyagorafael57@gmail.com>
2024-11-05 20:06:07 -0300

    refactor: move Logo.tsx to new directory

--- src/components/Logo.tsx
+++
@@ -1,9 +0,0 @@
-import taskerLogo from "../assets/tasker-logo.png";
-
-export function Logo() {
-	return (
-		<figure className="logo">
-			<img src={taskerLogo} alt="Logo da Tasker" />
-		</figure>
-	);
-}
