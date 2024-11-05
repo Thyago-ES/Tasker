3543fa9fb83305ae1fbd45332298fecad3b2e9c2
Thyago-ES <thyagorafael57@gmail.com>
2024-11-05 19:33:10 -0300

    chore: add NavBar and Footer components

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import { Footer } from "../components/Footer";
-import { NavBar } from "../components/NavBar";
+import { Footer } from "../components/mainLayout/Footer";
+import { NavBar } from "../components/mainLayout/NavBar";
 
 export function MainLayout() {
 	return (
