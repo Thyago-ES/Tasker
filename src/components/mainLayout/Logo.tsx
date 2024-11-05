fa4d77de051cf01d31958480e1c727699637bfb5
Thyago-ES <thyagorafael57@gmail.com>
2024-11-05 19:26:11 -0300

    chore: fix folder structure

---
+++ src/components/mainLayout/Logo.tsx
@@ -0,0 +1,9 @@
+import taskerLogo from "../../assets/tasker-logo.png";
+
+export function Logo() {
+	return (
+		<figure className="logo">
+			<img src={taskerLogo} alt="Logo da Tasker" />
+		</figure>
+	);
+}
