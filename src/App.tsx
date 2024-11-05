57f9d217c1bf560c24b1656835d87726ef28560e
Thyago-ES <thyagorafael57@gmail.com>
2024-11-05 19:48:50 -0300

    chore: simple move style instance on app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { BrowserRouter as Router } from "react-router-dom";
 
-import "./App.css";
 import { AppRoutes } from "./routes";
 import { Container } from "./layout/Container";
+
+import "./styles/App.css";
 
 function App() {
 	return (
